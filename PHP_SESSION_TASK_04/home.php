5775b04a25013f86e5a0d6c2e08c18e03e04cf67
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 09:06:03 +0600

    home page

--- PHP_SESSION_TASK_04/home.php
+++ PHP_SESSION_TASK_04/home.php
@@ -1,21 +1,32 @@
+<?php
+	session_start();
+	?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Public Home</title>
+	<style>table{border-collapse:collapse;}</style>
 </head>
 <body>
 	<form>
-		<fieldset>
-			<table>
+		<table width="40%" cellpadding="5" border="2">
 				<tr>
-					<td colspan="3" align="right">
+					<td colspan="3">
+						
 						<a href="home.php">Home</a>
 						<a href="login.php">Login</a>
 						<a href="registration.php">Registration</a>
+						
 					</td>
 				</tr>
-			</table>
-		</fieldset>
+				<tr>
+					<td colspan="4"><h3>Welcome to xCompany</h3></td>
+				</tr>
+				<tr>
+					<td align="center">Copyright 2017</td>
+				</tr>
+		</table>
+		
 	</form>
 </body>
 </html>
