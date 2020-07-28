3bb6ac14fee311baf1f9aa65221faf893f255964
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:23:30 +0600

    Edit profile page

---
+++ PHP_SESSION_TASK_04/edit_profile.php
@@ -0,0 +1,81 @@
+<body>
+	<table border="1px" border="1" cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="80%" colspan="3">
+				<h2>Xcompany</h2>
+				<ul align="right">
+				
+					Logged in as<a href=""> Bob </a> |
+					<a href="login.html"> Logout </a>
+				</ul>
+			</td>
+		</tr>
+
+		<tr height="400px">
+			<td>
+				<ul>
+					<li><a href=""> Dashboard </a></li>
+					<li><a href=""> View Profile </a></li>
+					<li><a href=""> Edit Profile </a></li>
+					<li><a href=""> Change Profile Picture </a></li>
+					<li><a href=""> Change Password</a></li>
+					<li><a href=""> Logout</a></li>
+				</ul>
+			</td>
+			<td width="60%">
+				<form>
+					<fieldset>
+							<legend>EDIT PROFILE</legend>
+							<form>
+			
+								<table width="60%" >
+									<tr>
+										<td>Name</td>
+				
+										<td>:</td>
+										<td><input type="text" name=""></td>
+									</tr>
+									<tr>
+										<td>Email</td>
+										
+										<td>:</td>
+										<td><input type="email" name="" placeholder="sample@gmail.com"></td>
+									</tr>
+									<tr>
+										<td>Gender</td>
+										
+										<td>:</td>
+										<td><input type="radio" name=""> Male
+											<input type="radio" name=""> Female
+											<input type="radio" name=""> Other
+										</td>
+									</tr>
+									<tr>
+										<td>Date Of Birth</td>
+										
+										<td>:</td>
+										<td><input type="text" name=""></td>
+									</tr>
+									<tr>
+										<td></td>
+										<td></td>
+										<td>(dd/mm/yyyy)</td>
+									</tr>
+								</table>
+								<hr/>
+									<input type="submit" name="">
+							</form>
+					</fieldset>
+
+
+
+
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">copyright @2017</td>
+		</tr>
+	</table>
+</body>
+				
