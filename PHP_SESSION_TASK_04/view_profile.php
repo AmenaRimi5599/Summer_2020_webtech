209c9433caae756d3b7c200640b2ed2d13818024
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:22:30 +0600

    view profile page

---
+++ PHP_SESSION_TASK_04/view_profile.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title></title>
+</head>
+<body>
+ <table border="1px" border="1" cellpadding="5" cellspacing="0" align="center">
+ <tr >
+   <td colspan="2" width="80%"><h2>Xcompany</h2>
+     <ul align="right">
+       <a href="home.php">Home</a>|
+       <a href="login.php">Login</a>|
+       <a href="registration.php">Registration</a>
+     </ul>
+   </td>
+ </tr>
+
+ <tr height="400px">
+  <td>
+    <ul>
+      <li><a href=""> DashBoard </a></li>
+      <li><a href=""> View Profile </a></li>
+      <li><a href=""> Edit Profile </a> </li>
+      <li><a href=""> Change Profile Picture </a></li>
+      <li><a href=""> Change Password </a></li>
+      <li><a href=""> Logout </a> </li>
+    </ul>
+  </td>
+  <td width="80%">
+    <form>
+      <fieldset>
+    <legend><b>PROFILE</b></legend>
+  <form>
+    <br/>
+    <table cellpadding="0" cellspacing="0">
+      <tr>
+        <td>Name</td>
+        <td>:</td>
+        <td>Bob</td>
+        <td rowspan="7" align="center">
+          <img width="128" src="user.png"/>
+                    <br/>
+                    <a href="picture.html">Change</a>
+        </td>
+      </tr>   
+      <tr><td colspan="3"><hr/></td></tr>
+      <tr>
+        <td>Email</td>
+        <td>:</td>
+        <td>bob@aiub.edu</td>
+      </tr>   
+      <tr><td colspan="3"><hr/></td></tr>     
+      <tr>
+        <td>Gender</td>
+        <td>:</td>
+        <td>Male</td>
+      </tr>
+      <tr><td colspan="3"><hr/></td></tr>
+      <tr>
+        <td>Date of Birth</td>
+        <td>:</td>
+        <td>19/09/1998</td>
+      </tr>
+    </table>  
+        <hr/>
+        <a href="edit_profile.html">Edit Profile</a> 
+  </form>
+</fieldset>
+
+
+
+       </form>
+  </td>
+<tr>
+  <td colspan="2" align="center">copyright@2017</td>
+</tr>
+</table>
+
+</body>
+</html>
