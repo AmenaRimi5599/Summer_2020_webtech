39fe2835bdce23786e1c771d086241a4fe54f493
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:25:36 +0600

    password change page

---
+++ PHP_SESSION_TASK_04/change_password.php
@@ -0,0 +1,64 @@
+<body>
+    <table border="1px" border="1" cellspacing="0" cellpadding="5" align="center">
+        <tr>
+            <td width="80%" colspan="3">
+                <h2>Xcompany</h2>
+                <ul align="right">
+                
+                    Logged in as<a href=""> Bob </a> |
+                    <a href="login.html"> Logout </a>
+                </ul>
+            </td>
+        </tr>
+
+        <tr height="400px">
+            <td>
+                <ul>
+                    <li><a href=""> Dashboard </a></li>
+                    <li><a href=""> View Profile </a></li>
+                    <li><a href=""> Edit Profile </a></li>
+                    <li><a href=""> Change Profile Picture </a></li>
+                    <li><a href=""> Change Password</a></li>
+                    <li><a href=""> Logout</a></li>
+                </ul>
+            </td>
+            <td width="60%">
+                <form>
+                    <fieldset>
+                            <legend><b>CHANGE PASSWORD</b></legend>
+                            <form>
+                                <table>
+                                    <tr>
+                                        <td><font size="3">Current Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" /></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td><font size="3" color="green">New Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" /></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td><font size="3" color="red">Retype New Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" /></td>
+                                        <td></td>
+                                    </tr>
+                                </table>
+                                <hr />
+                                 <input type="submit" value="Submit" />
+                            </form>
+                </fieldset>
+
+
+            </form>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" colspan="2">copyright @2017</td>
+        </tr>
+    </table>
+</body>
+                
