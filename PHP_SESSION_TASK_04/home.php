579df65eb663469ca4777f982c1740f410958f12
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:16:05 +0600

    home page

--- PHP_SESSION_TASK_04/home.php
+++ PHP_SESSION_TASK_04/home.php
@@ -1,32 +1,37 @@
 <?php
 	session_start();
-	?>
+
+	//if($_SESSION['status']=="Ok")
+		?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Public Home</title>
-	<style>table{border-collapse:collapse;}</style>
+	
 </head>
 <body>
 	<form>
-		<table width="40%" cellpadding="5" border="2">
+		<table width="40%" cellpadding="5" border="2" cellspacing="0">
 				<tr>
-					<td colspan="3">
+					<td colspan="3" ><h2>Xcompany</h2>
+						<ul align="right">
 						
 						<a href="home.php">Home</a>
 						<a href="login.php">Login</a>
 						<a href="registration.php">Registration</a>
+					</ul>
 						
 					</td>
 				</tr>
 				<tr>
-					<td colspan="4"><h3>Welcome to xCompany</h3></td>
+					<td><h3>Welcome to xCompany</h3></td>
 				</tr>
 				<tr>
-					<td align="center">Copyright 2017</td>
+					<td align="center">Copyright @2017</td>
 				</tr>
 		</table>
 		
 	</form>
 </body>
 </html>
+
