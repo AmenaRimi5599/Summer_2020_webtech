8c5b9bad74b0019fd40008d3d774a63becdcb9aa
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:21:18 +0600

    logged in dashboard

---
+++ PHP_SESSION_TASK_04/dashboard.php
@@ -0,0 +1,34 @@
+<body>
+	<table border="1px" border="1" cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="80%" colspan="3">
+				<h2>Xcompany</h2>
+				<ul align="right">
+				
+					Logged in as<a href=""> Bob </a> |
+					<a href="login.html"> Logout </a>
+				</ul>
+			</td>
+		</tr>
+
+		<tr height="400px">
+			<td>
+				<ul>
+					<li><a href=""> Dashboard </a></li>
+					<li><a href=""> View Profile </a></li>
+					<li><a href=""> Edit Profile </a></li>
+					<li><a href=""> Change Profile Picture </a></li>
+					<li><a href=""> Change Password</a></li>
+					<li><a href=""> Logout</a></li>
+				</ul>
+			</td>
+			<td width="80%">
+				<h3>Welcome Bob</h3>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">copyright @2017</td>
+		</tr>
+	</table>
+</body>
+				
