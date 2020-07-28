d89b61ca72288790884814e557a24f16ede8a9ef
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:19:59 +0600

    forgot pass page

---
+++ PHP_SESSION_TASK_04/forgot_password.php
@@ -0,0 +1,41 @@
+
+<body>
+	<table width="60%"  border="1" cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="80%" colspan="2">
+				<h2>Xcompany</h2>
+				<ul align="right">
+				
+					<a href="home.html">Home</a> |
+					<a href="login.html"> Login</a> |
+					<a href="registration.html"> Registration</a>
+				</ul>
+			</td>
+		</tr>
+
+		<tr >
+			
+			<td  height="300px">
+				<form>
+				<fieldset>
+					<legend>FORGOT PASSWORD</legend>
+					<table>
+						<tr>
+							<td>Enter Email:</td>
+							<td><input type="email" name="email"></td>
+						</tr>
+						<tr>
+							<td><input type="submit" name="Submit"></td>
+						</tr>
+					</table>
+				</fieldset>
+			</form>
+	
+
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">copyright @2017</td>
+		</tr>
+	</table>
+</body>
