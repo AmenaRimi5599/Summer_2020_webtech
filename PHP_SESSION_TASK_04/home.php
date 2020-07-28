a1e724160f8be7c106d340ed6307033be9fd2442
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 08:48:40 +0600

    form_session

---
+++ PHP_SESSION_TASK_04/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Public Home</title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<table>
+				<tr>
+					<td colspan="3" align="right">
+						<a href="home.php">Home</a>
+						<a href="login.php">Login</a>
+						<a href="registration.php">Registration</a>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html>
