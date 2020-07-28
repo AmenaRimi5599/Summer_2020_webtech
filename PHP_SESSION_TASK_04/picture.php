ff3360f71d2b903be0605c89be734af210ef29e0
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-07-28 22:24:37 +0600

    Profile picture page

---
+++ PHP_SESSION_TASK_04/picture.php
@@ -0,0 +1,45 @@
+<body>
+	<table border="1px" border="1" cellspacing="0" cellpadding="5" align="center">
+		<tr>
+			<td width="80%" colspan="3">
+				<h2>Xcompany</h2>
+				<ul align="right">
+				
+					Logged in as<a href=""> Bob </a> |
+					<a href="login.html"> Logout </a>
+				</ul>
+			</td>
+		</tr>
+
+		<tr height="400px">
+			<td>
+				<ul>
+					<li><a href=""> Dashboard </a></li>
+					<li><a href=""> View Profile </a></li>
+					<li><a href=""> Edit Profile </a></li>
+					<li><a href=""> Change Profile Picture </a></li>
+					<li><a href=""> Change Password</a></li>
+					<li><a href=""> Logout</a></li>
+				</ul>
+			</td>
+			<td width="80%">
+				<form>
+					<fieldset>
+    						<legend><b>PROFILE PICTURE</b></legend>
+    						<form>
+        						<img width="128" src="user.png" />
+        						<br />
+       							 <input type="file">
+        						<hr />
+        						<input type="submit" value="Submit">
+	    						</form>
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" colspan="2">copyright @2017</td>
+		</tr>
+	</table>
+</body>
+				
