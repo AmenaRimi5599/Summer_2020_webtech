89583118e6a391f2468e39336bafc8f924a86f9f
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-04 08:55:35 +0600

    login

---
+++ LAB_TASK_EXAM_05/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+</head>
+<body>
+	<form action="logcheck.php" method="post">
+  <fieldset width="500px">
+    <legend>LOGIN</legend>
+    <table>
+      <tr>
+        <td>User Id</td>
+        <td>: <input type="text" name="userId"></td>
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td>: <input type="Password" name="pass"></td>
+      </tr>
+    </table>
+    <hr>
+    <input type="submit" name="submit"><a href="registration.php">Register</a>
+  </fieldset>
+</form>
+</body>
+</html>
