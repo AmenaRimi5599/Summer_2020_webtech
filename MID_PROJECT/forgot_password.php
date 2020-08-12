5ad47e391aeb751aa0633a345ce28dbd04c7a56c
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-12 21:49:15 +0600

    forgot password

---
+++ MID_PROJECT/forgot_password.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<
+	<body>
+    <table border="1px" border="1" cellspacing="0" cellpadding="5" align="center">
+     <tr>
+    <td width="40%"><h1>X HOTEL</h1> 
+      <ul align="right">
+        <a href="home.php">Home</a>|
+        <a href="login.php">Login</a>|
+        <a href="registration.php">Registration</a>
+      </ul>
+    </td>
+  </tr>
+        <tr>
+            <!--<td>
+                <ul>
+                    <li><a href="dashboard.php"> Dashboard </a></li>
+                    <li><a href="view_profile.php"> View Profile </a></li>
+                    <li><a href="edit_profile.php"> Edit Profile </a></li>
+                    <li><a href="picture.php"> Change Profile Picture </a></li>
+                    <li><a href="change_password.php"> Change Password</a></li>
+                    <li><a href="logout.php"> Logout</a></li>
+                </ul>
+            </td>-->
+            <td width="40%">
+                <form>
+                    <fieldset>
+                            <legend><b>CHANGE PASSWORD</b></legend>
+                            <form>
+                                <table>
+                                    <tr>
+                                        <td><font size="3">Current Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" name="currPassword" /></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td><font size="3" color="green">New Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" name="password" /></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td><font size="3" color="red">Retype New Password</font></td>
+                                        <td>:</td>
+                                        <td><input type="password" name="rtpassword" /></td>
+                                        <td></td>
+                                    </tr>
+                                </table>
+                                <hr />
+                                 <input type="submit" value="submit" />
+                            </form>
+                </fieldset>
+
+
+            </form>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" colspan="2">copyright @2017</td>
+        </tr>
+    </table>
+
+</body>
+</html>
