ba4c918d5a61b2f0f4b90dcfe357f490823115f2
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-23 23:40:15 +0600

    update

--- FINAL_LAB_TASK_02/name/name/name.php
+++ FINAL_LAB_TASK_02/name/name/name.php
@@ -5,10 +5,10 @@
   <script type="text/javascript" src="name.js"></script>
 </head>
 <body>
-  <form action="name.php" onsubmit="return validateName()" method="post">
-  	<?=$_POST['name']?>
+  <form action="namecheck.php" onsubmit="return validateName()" method="post">
+  	
   	<fieldset>
-  		
+
   		 Name:  <input type="text" id="name1" name="name"><br>
                 <input type="submit" name="submit" >
   	</fieldset>
