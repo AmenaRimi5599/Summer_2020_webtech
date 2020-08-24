5e1fceeea447821a2a4d5799ea3a888e5784e911
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-24 23:09:12 +0600

    validate dateOfBirth

--- FINAL_LAB_TASK_02/dob/dob.js
+++ FINAL_LAB_TASK_02/dob/dob.js
@@ -4,26 +4,26 @@ function validateDOB()
    var Id2 = document.getElementById('id2').value;
    var Id3 = document.getElementById('id3').value;
 
-   if(Id1=="" || Id2 =="" ||Id3 =="")
-   {
-    alert("Invalid Date of Birth");
-       return false;
-   }
-   if(!(Id1>0 && Id1<31))
-    {
-    alert("Invalid");
-    return false;
-   }
-   if(!(Id2>0 &&Id2<13))
-   {
-    alert("Invalid");
-    return false;
-   }
-   if
-    (!(Id3>1900 &&Id2<2016)){
-    alert("Invalid");
-    return false;
-   }
+      if(Id1=="" || Id2 =="" ||Id3 =="")
+        {
+          alert("Invalid Date of Birth");
+          return false;
+        }
+      if(!(Id1>0 && Id1<31))
+        {
+          alert("Invalid !!");
+          return false;
+        }
+      if(!(Id2>0 &&Id2<13))
+        {
+          alert("Invalid !!");
+          return false;
+        }
+      if(!(Id3>1900 &&Id2<2016))
+      {
+        alert("Invalid !!");
+        return false;
+      }
    
    
 
