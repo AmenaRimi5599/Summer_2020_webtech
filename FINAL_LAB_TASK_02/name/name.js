182194b9cd786437ba37eaac78ec2f8c9d7517ce
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-25 11:27:41 +0600

    update

--- FINAL_LAB_TASK_02/name/name.js
+++ FINAL_LAB_TASK_02/name/name.js
@@ -1,29 +1,55 @@
 function validateName()
 {
-	var fname = document.getElementById('name1').value;
-	var n = fname.length;
-	var letters = /^[A-Za-z.-]+$/;
+
+
+
+var fname = document.getElementById('name1').value;
+  //var name = false;
+	//var n = fname.length;
+	//var letters = /^[A-Za-z.-]+$/;
 
 	if (fname == "")
 	{
 		alert("name must be filled up");
+    //document.getElementById("nmsg").innerHTML="Field cannot be empty !!";
+		//name = false;
 		return false;
 	}
-
-	if(n < 2)
-	{
-		alert("name should be more than 2 character");
-		return false;
-	}
-
-	if(fname.match(letters))
-	{
-		return true;
-	}
-	else
-	{
-		alert("Invalid Name");
-		return false;
-	}
-
+  else
+  {
+    if((fname[0]>='A' && fname[0]<='Z') || (fname[0]>='a' && fname[0]<='z'))
+    {
+      if(fname.length>=2)
+      {
+        var i=0;
+        while(i<fname.length)
+        {
+          if(!((fname[i]>='A' && fname[i]<='Z') || (fname[i]>='a' && fname[i]<='z') || fname[i]=='.' || fname[i]=='-' ))
+          {
+            alert("Invalid");
+            //document.getElementById("nmsg").innerHTML="name should contain A-Z or a-z";
+            //name = false;
+            return false;
+          }
+          i=i+1;
+        }
+        return true;
+      }
+      else
+      {
+        alert("name too short");
+        //document.getElementById('nmsg').innerHTML="name too short";
+        //name = false;
+        return false;
+      }
+    }
+    else
+    {
+      alert("Invalid");
+     // document.getElementById('nmsg').innerHTML="Invalid";
+      //name = false;
+      return false;
+    }
+  }
 }
+  //return false;
