f279efd39093f1f5dcc16ba847141f6672a71304
AmenaRimi <67853864+AmenaRimi5599@users.noreply.github.com>
2020-08-26 09:43:15 +0600

    update dob

--- FINAL_LAB_TASK_02/dob/dob.js
+++ FINAL_LAB_TASK_02/dob/dob.js
@@ -1,30 +1,52 @@
 function validateDOB()
 {
-   var Id1 = document.getElementById('id1').value;
-   var Id2 = document.getElementById('id2').value;
-   var Id3 = document.getElementById('id3').value;
+   var id1 = document.getElementById('id1').value;
+   var id2 = document.getElementById('id2').value;
+   var id3 = document.getElementById('id3').value;
 
-      if((Id1 =="" ) && (Id2 =="") && (Id3 ==""))
+      if((id1 !="" ) && (id2 !="") && (id3 !=""))
         {
-          alert("Invalid Date of Birth");
-          return false;
+          if((id1>0 && id1<32) && (id2>0 && id2<13) && (id3>1990 && id3<2016))
+            {
+              document.getElementById('check').innerHTML=""
+              return true;
+            }
+            else
+            {
+              document.getElementById('check').innerHTML="Invalid date or month or year";
+              return false;
+            }
         }
-      if(!(Id1>0 && Id1<31))
+        else
         {
-          alert("Invalid !!");
-          return false;
+              document.getElementById('check').innerHTML="Field can not be empty";
+              return false;
+
         }
-      if(!(Id2>0 &&Id2<13))
-        {
-          alert("Invalid !!");
-          return false;
-        }
-      if(!(Id3>1900 &&Id2<2016))
-      {
-        alert("Invalid !!");
-        return false;
-      }
+    }
+
+
+
+
+
+
+
+//       if(!(Id1>0 && Id1<31))
+//         {
+//           alert("Invalid !!");
+//           return false;
+//         }
+//       if(!(Id2>0 &&Id2<13))
+//         {
+//           alert("Invalid !!");
+//           return false;
+//         }
+//       if(!(Id3>1900 &&Id2<2016))
+//       {
+//         alert("Invalid !!");
+//         return false;
+//       }
    
    
 
-}
+// }
